84d1125fe2f575a2b61623b0156728fc7578fb40
Glauber Cini <133910134+glaubercini@users.noreply.github.com>
2023-09-09 08:46:56 -0300

    AdventureWorks2019 new script

--- install.sql
+++ install.sql
@@ -1,7 +1,12 @@
 USE [master]
-RESTORE DATABASE [AdventureWorks] 
-    FROM DISK = '/var/opt/mssql/backup/AdventureWorks2019.bak'
-        WITH REPLACE,
-        MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks.mdf',
-        MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks_log.ldf'
+
+RESTORE FILELISTONLY FROM DISK = N'/var/opt/mssql/backup/AdventureWorks2019.bak'
+GO
+
+RESTORE DATABASE AdventureWorks2019 FROM
+    DISK = N'/var/opt/mssql/backup/AdventureWorks2019.bak'
+    WITH REPLACE,
+    MOVE 'AdventureWorks2019' TO '/var/opt/mssql/data/AdventureWorks.mdf',
+    MOVE 'AdventureWorks2019_log' TO '/var/opt/mssql/data/AdventureWorks_log.ldf'
+    
 GO
